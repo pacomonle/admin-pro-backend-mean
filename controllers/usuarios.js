33d291cb4bfd942f54b7c7685ddf45f87d9cf43c
pacomonle <franciscomonleonleal@gmail.com>
2020-09-10 16:33:24 +0200

    ajustes

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -123,8 +123,15 @@ const actualizarUsuario = async (req, res = response) => {
                 });
             }
         }
-        
-        campos.email = email;
+        // validacion google
+        if ( !usuarioDB.google ){
+            campos.email = email;
+        } else if ( usuarioDB.email !== email ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario de google no pueden cambiar su correo'
+            });
+        }
         const usuarioActualizado = await Usuario.findByIdAndUpdate( uid, campos, { new: true } );
 
         res.json({
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const generarJWT = ( uid, role ) => {
 // tenemos que devolver una promesa ya que crear usuario y login son async y necesitamos un await
     return new Promise( ( resolve, reject ) => {
-        console.log(uid)
+       // console.log(uid)
         const payload = {
             uid,
             role
